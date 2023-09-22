21e8fc40b29c0718816f3173168e0fbecca2f6d4
dhicks6 <dhicks23@msudenver.edu>
2023-09-22 12:14:40 -0600

    Set up the database in mydb.py

--- dcrm/mydb.py
+++ dcrm/mydb.py
@@ -3,3 +3,20 @@
 # pip install mysql
 # pip install mysql-connector
 # pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '1234',
+
+    )
+
+# Prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
